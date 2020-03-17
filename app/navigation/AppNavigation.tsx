71d29ec7a96f32dd73cf7665645c9e035723c7ff
mauro <mauro@mauro.maurohome>
2020-03-17 17:00:58 -0300

    modified navigation and add listener functionality

---
+++ app/navigation/AppNavigation.tsx
@@ -0,0 +1,7 @@
+import {createAppContainer} from 'react-navigation';
+
+import Navigation from './Navigation';
+
+const App = createAppContainer(Navigation);
+
+export default App;
--- app/navigation/Navigation.tsx
+++ app/navigation/Navigation.tsx
@@ -5,15 +5,20 @@ import SignIn from '../auth/components/SignIn';
 import HomeLogin from '../auth/components/HomeLogin';
 import SignUp from '../auth/components/SignUp';
 import ForgotPassword from '../auth/components/ForgotPassword';
+import HomeApp from '../home/components/HomeApp';
 const authStack = createStackNavigator({
   HomeLogin,
   SignIn,
   SignUp,
   ForgotPassword,
 });
+const HomeAppStack = createStackNavigator({
+  HomeApp,
+});
 
 const rootSwitch = createSwitchNavigator({
   authStack,
+  HomeAppStack,
 });
 
 export default rootSwitch;
